744be2e78d0705764330daaf5ea5ceaed3184e30
harimu98-svg <harimu98@gmail.com>
2025-11-24 12:07:37 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -724,6 +724,9 @@ loadStockManagement() {
                         <button class="report-tab whitespace-nowrap px-4 py-2 border-b-2 border-transparent font-medium text-sm text-gray-500 hover:text-gray-700 hover:border-gray-300 hover:bg-gray-50 rounded-t" data-tab="pemasukan-pengeluaran">
                             💸 Pemasukan & Pengeluaran
                         </button>
+						<button class="report-tab whitespace-nowrap px-4 py-2 border-b-2 border-transparent font-medium text-sm text-gray-500 hover:text-gray-700 hover:border-gray-300 hover:bg-gray-50 rounded-t" data-tab="order-transaksi">
+ 					   📋 Order Transaksi
+						</button>
                         <button class="report-tab whitespace-nowrap px-4 py-2 border-b-2 border-transparent font-medium text-sm text-gray-500 hover:text-gray-700 hover:border-gray-300 hover:bg-gray-50 rounded-t" data-tab="transaksi-cancel">
                             ❌ Transaksi Cancel
                         </button>
